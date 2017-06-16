0f1b06eb6574bf01ea1f6703e6b7ee60e9329c90
Ramesh Ayyagari (CS) <ramesh_ayyagari@symantec.com>
2017-06-15 22:17:14 -0700

    Delete/reorg code error

--- monitor/files/gw-monitor.py
+++ monitor/files/gw-monitor.py
@@ -90,7 +90,7 @@ class monitorDb:
     def set_consec_error_status(self, module, state, consec_error_threshold):
         flags_array = [False]*int(consec_error_threshold)
         consec_key = 'consec_errors:'+str(self._instance)
-
+        consec_index = self._rh_status.hget(consec_key,'index')
         consec_index %= int(consec_error_threshold)
         flags_array = string_to_bool(self._rh_status.hmget(consec_key,self._status_flag_dict.keys()))
         flags_array[consec_index] = state
