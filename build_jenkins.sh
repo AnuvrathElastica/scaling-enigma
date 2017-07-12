5b828409584d8033d5ab84d4dc2a2fb2d419f040
Ramesh Ayyagari (CS) <ramesh_ayyagari@symantec.com>
2017-07-12 12:08:08 -0700

    Rinse and Repeat

--- build_jenkins.sh
+++ build_jenkins.sh
@@ -10,11 +10,15 @@ fi
 if [ -z $version ]; then
     echo "Bailing out VERSION $MGW_MONITOR_DOCKER_IMAGE_VERSION is null"
     exit 1
+else
+    echo "Building with version = $version"
 fi
 if [ -z ${MGW_MONITOR_DOCKER_IMAGE_TAG} ]; then
-    echo "Bailing out TAG $MGW_MONITOR_DOCKER_IMAGE_TAG is null"
+    echo "Bailing out TAG ${MGW_MONITOR_DOCKER_IMAGE_TAG} is null"
     printenv
     exit 1
+else
+    echo "If build successful will tag it with ${MGW_MONITOR_DOCKER_IMAGE_TAG}"
 fi
 
 docker_image_tag="${MGW_MONITOR_DOCKER_IMAGE_TAG}/mgw-docker"
