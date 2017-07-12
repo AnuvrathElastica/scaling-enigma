78d51d39dd07409404a9ef7771074ca50639fe4f
Ramesh Ayyagari (CS) <ramesh_ayyagari@symantec.com>
2017-07-12 10:36:12 -0700

    Fixed the jenkins job and the yml files. Next thing is to make this yml a part of the AMI

--- build_jenkins.sh
+++ build_jenkins.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-if [ $1 == "dev" ] ; then
-    tag="730926784978.dkr.ecr.us-west-2.amazonaws.com/dev/el-mgw-docker-monitor"
+tag=${MGW_MONITOR_DOCKER_IMAGE_TAG}
+
+if [ ${BRANCH_NAME} == 'master' ]; then
+    ver = 'latest'
 else
-    tag="730926784978.dkr.ecr.us-west-2.amazonaws.com/release/el-mgw-docker-monitor"
+    ver=${MGW_MONITOR_DOCKER_IMAGE_VERSION}
 fi
-ver=1.0
-dirs="monitor websvr redis syslogdocker"
-for dir in $dirs; do 
+
+dirs="monitor websvr"
+for dir in $dirs; do
     pushd $dir
     echo "$PWD building..."
-    ./build_docker_image.sh  $ver
-    echo "sudo docker tag $dir:latest $tag-$dir:$ver"
-    sudo docker tag $dir:latest $tag-$dir:$ver
-    echo "sudo docker push $tag-$dir:$ver"
-    sudo docker push $tag-$dir:$ver
+    ./build_docker_image.sh
+    echo "sudo docker tag $dir:$ver $tag/mgw-docker-$dir:$ver"
+    sudo docker tag $dir:$ver "$tag/mgw-docker-$dir:$ver"
+    echo "sudo docker push $tag/mgw-docker-$dir:$ver"
+    sudo docker push "$tag/mgw-docker-$dir:$ver"
     popd
 done
