a094e4da9bcdbe369b2d19c0836f45a3209c4d85
Ramesh Ayyagari (CS) <ramesh_ayyagari@symantec.com>
2017-07-12 11:46:14 -0700

    Debugging the script as it fails Jenkins

--- build_jenkins.sh
+++ build_jenkins.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-tag=$MGW_MONITOR_DOCKER_IMAGE_TAG
 
 if [ $BRANCH='master' ]; then
-    ver='latest'
+    VERSION='latest'
 else
-    ver=$MGW_MONITOR_DOCKER_IMAGE_VERSION
+    VERSION=$MGW_MONITOR_DOCKER_IMAGE_VERSION
 fi
 
-if [ -z $tag ]; then
-    echo "Bailing out $MGW_MONITOR_DOCKER_IMAGE_TAG is null"
+if [ -z $MGW_MONITOR_DOCKER_IMAGE_TAG ]; then
+    echo "Bailing out TAG $MGW_MONITOR_DOCKER_IMAGE_TAG is null"
     exit 1
 fi
-if [ -z $ver ]; then
-    echo "Bailing out $MGW_MONITOR_DOCKER_IMAGE_VERSION is null"
+if [ -z $VERSION ]; then
+    echo "Bailing out VERSION $MGW_MONITOR_DOCKER_IMAGE_VERSION is null"
     exit 1
 fi
+
+docker_image_tag="$MGW_MONITOR_DOCKER_IMAGE_TAG/mgw-docker"
 
 dirs="monitor websvr"
 for dir in $dirs; do
     pushd $dir
     echo "$PWD building on branch $BRANCH_NAME"
     ./build_docker_image.sh || exit 1
-    echo "sudo docker tag $dir:$ver $tag/mgw-docker-$dir:$ver"
-    sudo docker tag $dir:$ver "$tag/mgw-docker-$dir:$ver" || exit 1
-    echo "sudo docker push $tag/mgw-docker-$dir:$ver"
-    sudo docker push "$tag/mgw-docker-$dir:$ver" || exit 1
+    echo "sudo docker docker_image_tag $dir:$VERSION $docker_image_tag-$dir:$VERSION"
+    sudo docker tag $dir:$VERSION "$docker_image_tag-$dir:$VERSION" || exit 1
+    echo "sudo docker push $docker_image_tag-$dir:$VERSION"
+    sudo docker push "$docker_image_tag/mgw-docker-$dir:$VERSION" || exit 1
     popd
 done
