5757c62d477e9aa5709d48c83215d0f808796106
Ramesh Ayyagari (CS) <ramesh_ayyagari@symantec.com>
2017-07-12 11:55:03 -0700

    Debugging  the issue of environment variables

--- build_jenkins.sh
+++ build_jenkins.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 
-if [ $BRANCH='master' ]; then
-    VERSION='latest'
+if [ ${BRANCH}='master' ]; then
+    version='latest'
 else
-    VERSION=$MGW_MONITOR_DOCKER_IMAGE_VERSION
+    version=$MGW_MONITOR_DOCKER_IMAGE_VERSION
 fi
 
-if [ -z $MGW_MONITOR_DOCKER_IMAGE_TAG ]; then
-    echo "Bailing out TAG $MGW_MONITOR_DOCKER_IMAGE_TAG is null"
-    exit 1
-fi
-if [ -z $VERSION ]; then
+if [ -z $version ]; then
     echo "Bailing out VERSION $MGW_MONITOR_DOCKER_IMAGE_VERSION is null"
     exit 1
 fi
+if [ -z ${MGW_MONITOR_DOCKER_IMAGE_TAG} ]; then
+    echo "Bailing out TAG $MGW_MONITOR_DOCKER_IMAGE_TAG is null"
+    exit 1
+fi
 
-docker_image_tag="$MGW_MONITOR_DOCKER_IMAGE_TAG/mgw-docker"
+docker_image_tag="${MGW_MONITOR_DOCKER_IMAGE_TAG}/mgw-docker"
 
 dirs="monitor websvr"
 for dir in $dirs; do
     pushd $dir
-    echo "$PWD building on branch $BRANCH_NAME"
-    ./build_docker_image.sh $VERSION || exit 1
-    echo "sudo docker tag $dir:$VERSION $docker_image_tag-$dir:$VERSION"
-    sudo docker tag $dir:$VERSION "$docker_image_tag-$dir:$VERSION" || exit 1
-    echo "sudo docker push $docker_image_tag-$dir:$VERSION"
-    sudo docker push "$docker_image_tag/mgw-docker-$dir:$VERSION" || exit 1
+    echo "$PWD building on branch ${BRANCH_NAME}"
+    ./build_docker_image.sh $version || exit 1
+    echo "sudo docker tag $dir:$version $docker_image_tag-$dir:$version"
+    sudo docker tag $dir:$version "$docker_image_tag-$dir:$version" || exit 1
+    echo "sudo docker push $docker_image_tag-$dir:$version"
+    sudo docker push "$docker_image_tag/mgw-docker-$dir:$version" || exit 1
     popd
 done
